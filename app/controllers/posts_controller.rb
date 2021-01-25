a20a0cef3daaaf7200d0d0478183f3c6dfdf8a2a
kazuhiro_takahashi <kazuhiro1214yu@gmail.com>
2021-01-25 01:26:26 +0000

    3章3.7終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  
+  def new
+  end 
+  
   def index
     @posts = Post.all
   end
@@ -6,4 +10,5 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end 
+  
 end
