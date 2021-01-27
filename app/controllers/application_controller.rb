fce43b387f641753ccb2f696b5fb6f6f353921f9
kazuhiro_takahashi <kazuhiro1214yu@gmail.com>
2021-01-27 07:28:25 +0000

    8章８．９終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception # 元々ある設定の行ですが、そのまま残してください
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = User.find(session[:user_id]) if session[:user_id]
+  end
 end
